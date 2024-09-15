436c69d42a34fe39b38667aa6abe48f97d0a25f8
KALMARCH1K <kuzakhmetov.elnar2018@yandex.ru>
2024-09-15 17:15:34 +0300

    Update introspection.py

--- introspection.py
+++ introspection.py
@@ -1,11 +1,10 @@
 def introspection_info(obj):
     obj_type = type(obj).__name__
-    attributes = dir(obj)
-    methods = [method for method in attributes if callable(getattr(obj, method))]
+    attributes = obj.__dir__()
+    attribute = dir(obj)
+    methods = [method for method in attribute if callable(getattr(obj, method))]
     module = obj.__class__.__module__
-    info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module}
-
+    info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module},
     return info
-
 number_info = introspection_info(42)
 print(number_info)
